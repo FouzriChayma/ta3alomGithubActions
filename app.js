d9fe010c968738c4a0899072264f9c52cf2cfd7c
FouzriChayma <chayma.fouzri@esprit.tn>
2025-02-15 17:58:01 +0100

    changes99999

--- app.js
+++ app.js
@@ -6,76 +6,62 @@ var logger = require('morgan');
 const http = require("http");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var bibliothequesRouter = require('./routes/bibliotheque')
-var livreRouter = require('./routes/livre')
+var bibliothequesRouter = require('./routes/bibliotheque');
+var livreRouter = require('./routes/livre');
 var app = express();
 
 // To connect to DB
 var mongo = require("mongoose");
 var mongoConn = require("./config/database.json");
 
-console.log("avant la demande de connection dans le code ");
+console.log("avant la demande de connection dans le code");
 
-// Function to connect to MongoDB with retries
-const connectToDb = async () => {
-  let attempts = 0;
-  const maxAttempts = 10;
-  while (attempts < maxAttempts) {
-    try {
-      await mongo.connect(mongoConn.url);
+mongo.connect(mongoConn.url)
+  .then(() => {
+    console.log("connected to db");
 
-      console.log("connected to db");
-      break;
-    } catch (error) {
-      console.error("Error connecting to DB, retrying...");
-      attempts++;
-      if (attempts < maxAttempts) {
-        await new Promise(resolve => setTimeout(resolve, 5000)); // Wait 5 seconds before retrying
-      } else {
-        console.error("Failed to connect to DB after several attempts");
-        process.exit(1); // Exit the app if we fail to connect
-      }
-    }
-  }
-};
+    // Only create server and start listening after DB connection is successful
+    const server = http.createServer(app);
 
-// Call the connection function
-connectToDb();
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'twig');
 
-// end connect
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-// Création du serveur HTTP
-const server = http.createServer(app);
+    app.use('/', indexRouter);
+    app.use('/users', usersRouter);
+    app.use('/bibliotheque', bibliothequesRouter);
+    app.use('/livre', livreRouter);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'twig');
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      next(createError(404));
+    });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    // error handler
+    app.use(function(err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/bibliotheque', bibliothequesRouter)
-app.use('/livre', livreRouter);
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    // Start the server now that MongoDB is connected
+    server.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  })
+  .catch((err) => {
+    console.log("error connecting to db", err);
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+console.log("apres la demande de connection dans le code ");
