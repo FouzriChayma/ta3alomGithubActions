e536a92d474be45b42d4564f0caba08f281a2d50
FouzriChayma <chayma.fouzri@esprit.tn>
2025-02-15 19:44:43 +0100

    efefzze

--- app.js
+++ app.js
@@ -3,67 +3,48 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const http = require('http');
-var mongoose = require('mongoose');
+const http = require("http");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var bibliothequesRouter = require('./routes/bibliotheque');
-var livreRouter = require('./routes/livre');
-var mongoConn = require('./config/database.json');
-
+var bibliothequesRouter = require('./routes/bibliotheque')
+var livreRouter = require('./routes/livre')
 var app = express();
-
-console.log('Avant la demande de connexion à la base de données');
-
-mongoose.connect(mongoConn.url)
-  .then(() => {
-    console.log('Connecté à la base de données');
-
-    // Configuration d'Express
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'twig');
-
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
-
-    app.use('/', indexRouter);
-    app.use('/users', usersRouter);
-    app.use('/bibliotheque', bibliothequesRouter);
-    app.use('/livre', livreRouter);
-
-    // Gestion des erreurs 404
-    app.use((req, res, next) => {
-      next(createError(404));
-    });
-
-    // Gestion des erreurs générales
-    app.use((err, req, res, next) => {
-      res.locals.message = err.message;
-      res.locals.error = req.app.get('env') === 'development' ? err : {};
-      res.status(err.status || 500);
-      res.render('error');
-    });
-
-    // Création du serveur HTTP
-    const PORT = process.env.PORT || 3003;  // Use port 3003
-    const server = http.createServer(app);
-
-    server.listen(PORT, '0.0.0.0', () => {
-      console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
-    });
-
-    server.on('listening', () => {
-      console.log(`Le serveur écoute bien sur le port ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.error('Erreur de connexion à la base de données', err);
-    process.exit(1); // Quitter en cas d'échec de connexion
-  });
-
-console.log('Après la demande de connexion à la base de données');
-
+//To connect to DB
+var mongo = require("mongoose");
+var mongoConn = require("./config/database.json");
+console.log("avant la demande de connection dans le code ");
+mongo.connect(mongoConn.url).then(() => {
+  console.log("connected to db");
+}).catch(() => {
+  console.log("error connecting to db");
+});
+console.log("apres la demande de connection dans le code ");
+//end connect
+// Création du serveur HTTP
+const server = http.createServer(app);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'twig');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/bibliotheque', bibliothequesRouter)
+app.use('/livre',livreRouter)
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 module.exports = app;
